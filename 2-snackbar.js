5979df3355e1b56d94d5903d92fbfe883c9ad31d
Vladyslav-Avrashov <vladisloveavrashov@gmail.com>
2025-03-17 21:52:19 +0000

    Deploying to gh-pages from @ Vladyslav-Avrashov/goit-js-hw-10@55237ffb7fd080b0fa7b0d630d0b0e7dfb9eccd5 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */import{i as s}from"./assets/vendor-njWUcVeN.js";const o=document.querySelector("form");o.addEventListener("submit",t=>{t.preventDefault();const{state:r}=t.target.elements,e=Number(t.target.elements.delay.value);return new Promise((m,i)=>{setTimeout(()=>{r.value==="fulfilled"&&m(e),i(e)},e)}).then(()=>s.success({message:`✅ Fulfilled promise in ${e}ms`})).catch(()=>s.error({message:`❌ Rejected promise in ${e}ms`}))});
+//# sourceMappingURL=2-snackbar.js.map
